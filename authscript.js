ab30664009baddf9dfdda320d41b26b79f514c7d
Mr_JaVa_ <rhysojones@outlook.com>
2016-11-18 23:27:04 +0000

    Update authscript.js

--- authscript.js
+++ authscript.js
@@ -1,4 +1,8 @@
-if(document.URL.indexOf("ID=KEY:c6feaccc14d72437b8de54a0c00162a0b2866589029b6d848f5be709ebf644c6REG:Qcraft.pw ") != -1) {
-  document.write("KEY:c6feaccc14d72437b8de54a0c00162a0b2866589029b6d848f5be709ebf644c6REG:Qcraft.pw ");
+if(document.URL.indexOf("ID=KEY:c6feaccc14d72437b8de54a0c00162a0b2866589029b6d848f5be709ebf644c6REG:Qcraft.pw") != -1) {
+  document.write("KEY:c6feaccc14d72437b8de54a0c00162a0b2866589029b6d848f5be709ebf644c6REG:Qcraft.pw");
+} else {
+}   
+if(document.URL.indexOf("ID=KEY:000000000001REG:Localhost") != -1) {
+  document.write("KEY:000000000001REG:Localhost");
 } else {
 }   
